e165fc9a7f6b33d4aee5e961c7f412f6e385d059
mrbanananator <z5312278@ad.unsw.edu.au>
2020-12-14 02:38:45 +0000

    Add welcome message button which fetches message

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -26,7 +26,8 @@ public class DataServlet extends HttpServlet {
 
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String welcome = "Hello Jared!";
         response.setContentType("text/html;");
-        response.getWriter().println("<h1>Hello Jared!</h1>");
+        response.getWriter().println(welcome);
     }
 }
